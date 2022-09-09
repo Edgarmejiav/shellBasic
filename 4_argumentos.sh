e86369165620790639b62155006d88ad4e816bde
edgarmejiav <edgarm_10@hotmail.com>
2022-09-09 08:59:13 -0500

    shell

---
+++ 4_argumentos.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+nombreCurso=$1
+horarioCurso=$2
+
+echo "El nombre del curso es: $nombreCurso dictado en el horario de $horarioCurso"
+echo "El número de parámetros enviados es: $#"
+echo "Los parámetros enviados son: $*"
